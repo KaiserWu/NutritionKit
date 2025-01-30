cf9b4f13380c449c2d3906759b832b3ac3d2d61d
Sen Wu <sen.wu@miele.com>
2025-01-30 14:39:52 +0100

    extended ai view

--- Sources/NutritionKit/Camera/NutritionLabelScannerAiView.swift
+++ Sources/NutritionKit/Camera/NutritionLabelScannerAiView.swift
@@ -9,6 +9,7 @@ public struct NutritionLabelScannerAiView: View {
     
     /// The cutout rectangle.
     @State var cameraRectangle: CameraRect = DefaultCameraOverlayView.defaultLabelCutoutRect
+    @State var isProcessingImage: Bool = false
     
     public init(image: Binding<UIImage?>) {
         self._image = image
@@ -25,9 +26,53 @@ public struct NutritionLabelScannerAiView: View {
     }
     
     func onImageCaptured(_ img: UIImage, _ buffer: CVPixelBuffer) {
-        image = img
-        self.reset()
-        presentationMode.wrappedValue.dismiss()
+        guard let cgImage = img.cgImage else {
+            self.reset()
+            return
+        }
+        
+        self.isProcessingImage = true
+        
+        // TODO: Add blur detection
+        Task {
+            await self.processCapturedImage(cgImage)
+        }
+    }
+    
+    func processCapturedImage(_ image: CGImage) async {
+        let scanner = NutritionLabelDetector(image: image)
+        do {
+            guard let (_, rect) = try await scanner.findNutritionLabel() else {
+                DispatchQueue.main.async {
+                    self.reset()
+                }
+                
+                return
+            }
+            
+            DispatchQueue.main.async {
+                withAnimation {
+                    self.cameraRectangle = .init(rect.topLeft, rect.topRight, rect.bottomLeft, rect.bottomRight)
+                }
+            }
+            
+            let label = try await scanner.scanNutritionLabel()
+            guard label.isValid else {
+                DispatchQueue.main.async {
+                    self.reset()
+                }
+                return
+            }
+            
+            DispatchQueue.main.async {
+                self.isProcessingImage = false
+                self.image = UIImage(cgImage: image)
+                self.presentationMode.wrappedValue.dismiss()
+            }
+        }
+        catch {
+            Log.nutritionKit.error("finding nutrition label failed: \(error.localizedDescription)")
+        }
     }
     
     public var body: some View {
